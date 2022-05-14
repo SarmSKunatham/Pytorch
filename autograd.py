7b8f91691cb4bdbf38720065e34cd423a323eef7
Sueptrakul Kunatham [Sarm] <87699705+SarmSKunatham@users.noreply.github.com>
2022-05-14 19:19:56 +0700

    up

--- autograd.py
+++ autograd.py
@@ -1,6 +1,17 @@
 import torch
 
 
-x = torch.randn(3)
+x = torch.randn(3, requires_grad=True)
 
 print(x)
+
+y = x+2
+
+print(y)
+
+z = y*y*2
+z = z.mean()
+print(z)
+
+z.backward() # dz/dx
+print(x.grad)
